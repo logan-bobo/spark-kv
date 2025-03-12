2e0859ccd986d6718e028be0799facc498165008
Logan Cox <mail@logan-cox.com>
2025-03-12 09:20:50 +0000

    feat: base wiring of project with initial KvStore and methods

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+pub struct KvStore;
+
+impl KvStore {
+    pub fn new() -> Self {
+        panic!()
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        panic!()
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        panic!()
+    }
+
+    pub fn remove(&mut self, key: String) {}
+}
